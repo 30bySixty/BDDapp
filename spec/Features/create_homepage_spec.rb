fb2b3773339f5946121bd693c64839bacb6c3319
30bySixty <tshestopalova@gmail.com>
2016-08-31 17:51:58 -0500

    add home page

--- spec/Features/create_homepage_spec.rb
+++ spec/Features/create_homepage_spec.rb
@@ -4,9 +4,8 @@ RSpec.feature "Creating Home Page" do
   scenario do
     visit "/"
 
-    click_link "Athletes Den"
-    click_link "Home"
-
+    expect(page).to have_link("Athletes Den")
+    expect(page).to have_link("Home")
     expect(page).to have_content("Workout Lounge")
   end
   
