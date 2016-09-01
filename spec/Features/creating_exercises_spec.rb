5d44a766eceed5a9c808dca987552e97b09c2de7
30bySixty <tshestopalova@gmail.com>
2016-09-01 12:07:12 -0500

    add exercise failure

--- spec/Features/creating_exercises_spec.rb
+++ spec/Features/creating_exercises_spec.rb
@@ -23,4 +23,23 @@ RSpec.feature "Creating Exercise" do
     exercise = Exercise.last #because latest entry will be last entry in the DB
     expect(page.current_path).to eq(user_exercise_path(@foo, exercise)) #this is in the context of a user and an exercise -> nested route so have to give the user and the exercise
   end
+  
+  scenario "with valid inputs" do
+    visit "/"
+    
+    click_link "My Lounge"
+    click_link "New Workout"
+    expect(page).to have_link("Back")
+    
+    fill_in "Duration", with: nil
+    fill_in "Workout Details", with: ""
+    fill_in "Activity Date", with: ""
+    click_button "Create Exercise"
+    
+    expect(page).to have_content("Exercise has not been created")
+    expect(page).to have_content("Duration in min can't be blank")
+    expect(page).to have_content("Duration in min is not a number")
+    expect(page).to have_content("Workout can't be blank")
+    expect(page).to have_content("Workout date can't be blank")
+  end
 end
